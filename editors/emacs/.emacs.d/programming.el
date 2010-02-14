6bedacf50650c9cc92086fdb8d42651c72bb4562
charles@drishti <charles@drishti>
2010-02-14 13:51:32 -0500

    removing matlab by default... should customize that requirement as needed

--- editors/emacs/.emacs.d/programming.el
+++ editors/emacs/.emacs.d/programming.el
@@ -1,13 +1,13 @@
 ;MATLAB
-(add-to-list 'load-path "~/.emacs.d/matlab-emacs")
-(require 'matlab-load)
+;(add-to-list 'load-path "~/.emacs.d/matlab-emacs")
+;(require 'matlab-load)
 
-(autoload 'matlab-mode "matlab" "Enter MATLAB mode." t)
-(setq auto-mode-alist (cons '("\\.m\\'" . matlab-mode) auto-mode-alist))
-(autoload 'matlab-shell "matlab" "Interactive MATLAB mode." t)
+;(autoload 'matlab-mode "matlab" "Enter MATLAB mode." t)
+;(setq auto-mode-alist (cons '("\\.m\\'" . matlab-mode) auto-mode-alist))
+;(autoload 'matlab-shell "matlab" "Interactive MATLAB mode." t)
 
 ;User Level customizations (You need not use them all):
-(setq matlab-indent-function-body t)  ; if you want function bodies indented
+;(setq matlab-indent-function-body t)  ; if you want function bodies indented
 
 ;(setq matlab-verify-on-save-flag nil) ; turn off auto-verify on save
 ;(defun my-matlab-mode-hook ()
