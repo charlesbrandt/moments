2eb8303aed8a14d517e107a2a2a205d444ac718f
charles@breathe <charles@breathe>
2010-03-16 11:18:29 -0400

    added license to all source files
    --HG--
    rename : moments/filter.py => scripts/filter.py

--- moments/path.py
+++ moments/path.py
@@ -1,3 +1,25 @@
+# ----------------------------------------------------------------------------
+# moments
+# Copyright (c) 2009-2010, Charles Brandt
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# ----------------------------------------------------------------------------
 """
 *2009.10.21 16:07:53 
 module for interacting with the filesystem.
@@ -16,6 +38,7 @@ from datetime import datetime
 
 from timestamp import Timestamp
 from journal import Journal
+from association import check_ignore
 
 from tags import Tags
 
