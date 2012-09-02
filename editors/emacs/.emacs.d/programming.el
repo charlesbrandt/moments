6b1cbd8e82cd52bb5d37b8079bdfe0f9eb9de396
Charles Brandt <charles.brandt@gmail.com>
2012-09-02 17:31:58 -0400

    adding in emacs mode for SASS highlighting

--- editors/emacs/.emacs.d/programming.el
+++ editors/emacs/.emacs.d/programming.el
@@ -11,11 +11,48 @@
 (add-to-list 'auto-mode-alist '("\\.dtml$" . html-mode))
 (add-to-list 'auto-mode-alist '("\\.mako$" . html-mode))
 
-(add-to-list 'auto-mode-alist '("\\.sass$" . css-mode))
-(add-to-list 'auto-mode-alist '("\\.scss$" . css-mode))
+;SASS
+;(load-file "~/.emacs.d/scss-mode.el")
+;;; scss-mode.el --- Major mode for editing SCSS files
+;;
+;; Author: Anton Johansson <anton.johansson@gmail.com> - http://antonj.se
+;; URL: https://github.com/antonj/scss-mode
+;; Created: Sep 1 23:11:26 2010
+;; Version: 0.5.0
+;; Keywords: scss css mode
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2 of
+;; the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be
+;; useful, but WITHOUT ANY WARRANTY; without even the implied
+;; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+;; PURPOSE.  See the GNU General Public License for more details.
+
+(defconst scss-font-lock-keywords
+  ;; Variables
+  '(("$[a-z_-][a-z-_0-9]*" . font-lock-constant-face)))
+
+(define-derived-mode scss-mode css-mode "SCSS"
+  "Major mode for editing SCSS files, http://sass-lang.com/
+Special commands:
+\\{scss-mode-map}"
+  (font-lock-add-keywords nil scss-font-lock-keywords)
+  ;; Add the single-line comment syntax ('//', ends with newline)
+  ;; as comment style 'b' (see "Syntax Flags" in elisp manual)
+  (modify-syntax-entry ?/ ". 124" css-mode-syntax-table)
+  (modify-syntax-entry ?* ". 23b" css-mode-syntax-table)
+  (modify-syntax-entry ?\n ">" css-mode-syntax-table)
+  ;(add-to-list 'compilation-error-regexp-alist scss-compile-error-regex)
+  ;(add-hook 'after-save-hook 'scss-compile-maybe nil t)
+  )
+
+(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
+(add-to-list 'auto-mode-alist '("\\.sass$" . scss-mode))
 
 ;call any other programming modes you need here:
-;(load-file "~/.emacs.d/programming.el")
 
 ;MATLAB
 ;(add-to-list 'load-path "~/.emacs.d/matlab-emacs")
