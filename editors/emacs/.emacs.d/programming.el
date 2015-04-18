cc63b85d0fe00c258d51b40e536fd1c575a5ef85
Charles Brandt <charles.brandt@gmail.com>
2015-04-17 20:36:10 -0400

    updates to emacs mode

--- editors/emacs/.emacs.d/programming.el
+++ editors/emacs/.emacs.d/programming.el
@@ -31,6 +31,17 @@
 (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.less$" . web-mode))
 
+(setq web-mode-engines-alist
+      '(("css" . "\\.less$")
+        )
+)
+
+(setq web-mode-content-types-alist
+  '(("css" . "\\.less$")
+    )
+)
+
+
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-css-indent-offset 2)
 (setq web-mode-code-indent-offset 2)
