fcde467d346d343dc181de2f586b7564c248a0d2
Charles Brandt <code@charlesbrandt.com>
2017-11-09 17:35:51 -0500

    adding in elpa modules for emacs. this allows them to work offline

---
+++ editors/emacs/.emacs.d/elpa/vue-html-mode-20170928.1057/vue-html-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "vue-html-mode" "20170928.1057" "Major mode for editing Vue.js templates" 'nil :commit "9218c61ff1ab2bdc6a6711d21ad760198d3511a8" :url "http://github.com/AdamNiederer/vue-html-mode" :keywords '("languages" "vue" "template"))
